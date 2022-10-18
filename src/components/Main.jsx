2e908c0023695ed7de23a7439822ce61aff0554e
Bella <Isabellapinkerman@gmail.com>
2022-10-18 07:51:29 -0700

    Posts component started

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from './';
 
 const Main = () => {
+
+  const [data, setData] = useState()
+  useEffect(()=>{
+  async function getUrl(){
+    let response = await fetch( `https://strangers-things.herokuapp.com/api/2209-FTB-ET_WEB_FT`)
+    let result = await response.json(
+      console.log(result),
+      setData(result)
+    )
+    getUrl()
+  }},[])
+
   return (
     <div id="main">
       <Navbar />
+      {/* <Posts data={data}/> */}
   </div>
   );
 };
